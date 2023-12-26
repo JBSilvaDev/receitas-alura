e8878773edecd46f0fb61faf0c648890a4c76895
JB Silva Dev <jbsilva.dev@outlook.com>
2023-12-26 11:00:27 -0300

    paginação no dashboard

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -1,13 +1,16 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
 from receitas.models import Receita
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
-
+    paginator = Paginator(receitas, 3) # Itens a serem paginados, quantidade por pagina
+    page = request.GET.get('page')
+    receitas_por_pagina = paginator.get_page(page)
     dados = {
-        'receitas' : receitas
+        'receitas' : receitas_por_pagina
     }
     return render(request,'index.html', dados)
 
